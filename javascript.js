ff9276e453dc49f5ff5882ba386db0c8b8bce2f3
Gomathibanu <149371793+Gomathibanu@users.noreply.github.com>
2025-07-16 13:26:56 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+const prompt=require("prompt-sync")();
+
+let mark=("enter the mark");
+console.log(mark);
+if(mark>80 & mark<=100);{
+console.log("grade:","A");
+}
+elseif(mark>60 & mark<=80);{
+    console.log("grade:","B");
+}
+elseif(mark>40 & mark<=60);{
+    console.log("grade:","c");
+}
