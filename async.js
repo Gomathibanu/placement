e1121758a86fe6fd350ebed73437b9b17ba7fca5
Gomathibanu <149371793+Gomathibanu@users.noreply.github.com>
2025-07-17 09:14:33 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,8 @@
+console.log("start");
+console.log("middle");
+console.log("end");
+console.log("start")
+setTimeout(() => {
+    console.log("this runs later");
+},2000);
+console.log("end")
---
+++ demo2.js
@@ -0,0 +1,23 @@
+/*function script(drama){
+    return " people " + drama;
+    
+}
+console.log(script(" doing practice"));   //output : people  doing practice
+*/
+
+/*const recipe=(food)=>{
+    return "i love "+ food;
+
+};
+let op=recipe("Biriyani")
+console.log(op);
+*/
+
+/*const add=(n)=>{
+    return n*n*n*n;
+};
+console.log(square(6));
+*/
+
+const square = n=>n*n;
+console.log(square(11));
