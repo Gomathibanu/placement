70471de0b92dba686c0cf0682b775ce6c0eba2bd
Gomathibanu <149371793+Gomathibanu@users.noreply.github.com>
2025-07-17 09:14:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var val=67;
+let val2=34;
+console.log(val);
+//console.log("45"==45);
+//console.log("45"===45);
+
+const prompt=require("prompt-sync")();
+
+//let age = prompt("enter the age");
+//let urName=prompt("enter your name:");
+
+console.log("age");
+console.log("urName");
+
+//console.log(val=="67");
+//console.log(val==="67");
+
+var age = prompt("enter the age");
+if(age >=70){
+   console.log("retired")
+}
+else{
+    console.log("working")
+}
+console.log(age)
